1d1a5013112679c1833667e1b24925c2534bdafe
David Jackson <altmail@jackson-wallace.net>
2017-02-14 20:16:24 -0600

    Expanded "Getting Started" and "Running Your App" pages.

--- src/components/development.vue
+++ src/components/development.vue
@@ -8,16 +8,63 @@
   <div>
     <h1>Running Your App</h1>
     <p>
-      To run your app for development, use the command listed below.
+      To run your app for development, use
     </p>
 
     <pre v-highlightjs><code class="bash">
     vue-build dev
-    vue-build dev --port=1324
-    vue-build dev --devtool="eval-source-map"
     </code></pre>
 
-    <h3>Error overlay</h3>
+    <p>This command starts a
+    <a href="https://webpack.js.org/configuration/dev-server/">webpack
+    development server</a> (on <code>localhost:8080</code> by default). Once you
+    load the app in your browser, any changes you make to your code will
+    trigger a recompile, and then automatically appear in your app.</p>
+
+    <p>In vue-build applications, the default <a href="https://webpack.js.org/concepts/entry-points/">entry point</a> is <code>src/app.js</code>. When
+    your application runs in the browser, the <strong>contents of this file are the
+    first thing to run</strong>. In the default project template, <code>src/app.js</code>
+    sets up a Vue app for you, and "mounts" it in the page inside of
+    <code class="html" v->
+      &lt;div id="app"&gt;&lt;/div&gt;
+    </code>.</p>
+
+    <h2>Writing components</h2>
+
+    <p>vue-build includes <a href="http://vue-loader.vuejs.org/en/">vue-loader</a>,
+    which allows you to load single-file components from <code>.vue</code> files.</p>
+
+    <p>If you haven't used vue-loader before, check out your project's
+    <code>app.vue</code> file for an example. For more details on the
+    <code>.vue</code> format, see <a href="http://vue-loader.vuejs.org/en/start/spec.html">the official spec</a>.</p>
+
+    <h2>Styling</h2>
+
+    <p>vue-build includes support for CSS and SCSS, though you can add other
+    styling languages by <router-link to="/webpack">overriding your webpack config</router-link>. To include a style in your project, just import it
+    from a Javascript file like so:</p>
+
+    <pre v-highlightjs><code class="javascript">
+    import './relative/path/to/some.scss'
+    </code></pre>
+
+    <p>You can use SCSS inside the <code class="html">&lt;style&gt;</code>
+    block of a <code>.vue</code> component file like so:</p>
+
+    <pre v-highlightjs><code class="html">
+      &lt;style lang="scss"&gt;
+        // SCSS will work in here now
+      &lt;/style&gt;
+    </code></pre>
+
+    <p>Note that styles in components don't share any variables, mixins, or
+    placeholders with other SCSS files in your project. If you want to bring
+    those things in, you'll need to <code class="scss">@import</code> them.
+    <strong>Make sure that you only import partials from your components.
+    </strong> If you import full styles, they'll be duplicated in every
+    component, bloating your build and slowing down development.</p>
+
+    <h2>Error overlay</h2>
     <p>
       During development if there are any linting, syntax errors or module import issues,
       they will be displayed as an overlay in the current running browser.
--- src/components/getting-started.vue
+++ src/components/getting-started.vue
@@ -10,17 +10,22 @@
     npm install -g vue-build
     </code></pre>
 
-    <h3>Creating a new application</h3>
+    <h2>Creating a new application</h2>
     <p>
-      By typing in the terminal the script below you will be asked a variation
-      of questions that will help you setup your application files and folders
+      Inside the folder that will contain your project, just type:
     </p>
     <pre v-highlightjs><code class="bash">
     vue-build init
     </code></pre>
+
+    <p>vue-build will ask a few questions about your project, and then
+    generate some files. Be sure to run <code class="bash">npm install</code>
+    afterward.</p>
     <p>
       <strong>Note:</strong> See <router-link to="/structure">folder structure</router-link> for more details
       on the minimum and maximum amount of files and folders needed to create an application.
     </p>
+
+    <p>Now, you're ready to <router-link to="/development">run the app</router-link>.</p>
   </div>
 </template>

c672833cc37b1d90e6326435efafcf6870fc0bcd
Brianvoe <brian@webiswhatido.com>
2017-02-20 15:15:10 -0600

    analytics - wrapped analytics in production mode only

--- src/google-analytics.js
+++ src/google-analytics.js
@@ -1,11 +1,13 @@
-/* global ga */
-window.addEventListener('load', () => {
-  ga('create', 'UA-91834289-1', 'auto')
+/* global ENVIRONMENT,ga */
+if (ENVIRONMENT === 'production') {
+  window.addEventListener('load', () => {
+    ga('create', 'UA-91834289-1', 'auto')
 
-  ga('require', 'maxScrollTracker')
-  ga('require', 'outboundLinkTracker')
-  ga('require', 'urlChangeTracker')
-  ga('require', 'pageVisibilityTracker')
+    ga('require', 'maxScrollTracker')
+    ga('require', 'outboundLinkTracker')
+    ga('require', 'urlChangeTracker')
+    ga('require', 'pageVisibilityTracker')
 
-  ga('send', 'pageview')
-})
+    ga('send', 'pageview')
+  })
+}
